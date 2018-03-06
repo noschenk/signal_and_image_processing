cb2d1f0187c4d5f812e7dd97e2bfc0364b137d8c
noschenk <noelle.schenk@students.unibe.ch>
2018-03-06 18:11:04 +0100

    finished homework 0

--- h0/hw_0_noelleschenk_demo.py
+++ h0/hw_0_noelleschenk_demo.py
@@ -41,8 +41,6 @@ def ex0(a, b, c):
 
 
 # Ex 0.2 ------------------------------------------------------------------------------------
-
-
 # 0.2.1
 # Write a function create image that generates an image with the height/width
 # dimensions of n x m with uniform randomly distributed black and white pixels.
@@ -92,16 +90,127 @@ plt.show()
 # 0.2.3
 # image img, compute euclidean distance of each white pixel from the red pixel without the use of any for loop.
 plt.imshow(test)  # the image to work with.
-# store the coordinates of white pixels
-whitepix = find_pixels((1,1,1), test)
-# store the coordinates of the red pixel.
-redpix = find_pixels((1,0,0),test)
-# to calculate the distance nicely, make an array of the shape of whitepix and just put the red pixels
-#    coordinates over and over in it.
-redpix_large = np.tile(redpix, (whitepix.shape[0],1))
-# calculate the distances between two matrices (arrays)
-dist = whitepix-redpix_large
-# take the euclidean distance
-edist = np.linalg.norm(dist, 1)
 
+
+def compute_distances(test):
+    # store the coordinates of white pixels
+    whitepix = find_pixels((1, 1, 1), test)
+    # store the coordinates of the red pixel.
+    redpix = find_pixels((1, 0, 0), test)
+    # calculate euclidean distance
+    dist = (whitepix - redpix) ** 2  # numpy automatically acts as if redpix had the same dimensions as whitepix!
+    dist = np.sum(dist, axis=1)  # sum square of coordinates together
+    dist = np.sqrt(dist)
+    return dist
+
+# test the function with the image 'test'
+dist = compute_distances(test)
+
+
+# ------------------------------------
 # 0.2.4
+# Display the computed distance vector dist in a histogram (with 100 bins),
+# compute the mean, standard deviation and the median. Display the values as
+# a title above the plot.
+
+
+def visualize_results(dist):
+    """outputs histogram of the computed distance vector dist"""
+    plt.hist(dist, bins=100)
+    plt.title('mean = %f, std = %f, median = %f' % (np.mean(dist), np.std(dist), np.median(dist)))
+    # %f for a float, the order is the same in the string and how variables are given
+    plt.show()
+
+# testing the function
+visualize_results(dist)
+# -------------------------------------------------------------------------------------------
+
+
+# Ex 0.3 ------------------------------------------------------------------------------------
+
+# import the image as stop
+stop = plt.imread('h0/stopturnsigns.jpg')
+# create copy of the image (if not, the image can not be changed...)
+stop = np.copy(stop)
+
+# find t_min and t_max that fulfill the desired things
+t_min = [210, 0, 0]
+t_max = [255, 85, 85]
+# pixels between t_min and t_max are red
+m = np.logical_and(stop[:, :, :] >= t_min, stop[:, :, :] <= t_max)
+# pixels must be in the range at the same time at all 3 color channels
+m = np.logical_and(m[:, :, 0], m[:, :, 1], m[:, :, 2])
+# copy[np.logical_not(m)] = [0, 0, 255]
+m = np.logical_not(m)
+m[140:, :] = True
+stop[m] = [0, 0, 255]
+plt.imshow(stop)
+plt.show()
+
+# # other way of doing it, but without t_min and t_max
+# # make the whole copied image blue
+# copy[:,:,:] = [0, 0, 255]
+# # add the stop sign back in
+# copy[30:140, 80:190, :] = stop[30:140, 80:190, :]
+# # setting values to blue which are 'not red enough'
+# test = np.logical_or(copy[:, :, 0] < 230, copy[:, :, 1] > 140, copy[:, :, 2] > 140)
+# copy[test] = [0,0,255]
+# -------------------------------------------------------------------------------------------
+
+
+# Ex 0.4 ------------------------------------------------------------------------------------
+# Considering two 1 dimensional arrays, calculate the mean square error between them.
+# Check your function for arrays of known difference
+# ------------------------------------
+# 0.4.1
+# 2 arrays x, y of length 100 with random values
+x = np.random.rand(100)
+y = np.random.rand(100)
+
+# 0.4.2
+
+
+def mse(a, b):
+    """calculation of mean square error"""
+    res = (1 / a.shape[0]) * np.sum((a - b)**2)
+    return res
+
+# 0.4.3
+mse(x, y)
+
+# 0.4.4
+mse(x, x)
+
+# 0.4.5
+# offset = verschiebung
+# expectation : I expect that mean square errors differ by 2, as all values are offset by 2.
+mse(x, x+2)
+# observation : the mse is 4, the square of 2.
+# test : if i calculate mse(x, x+3) the mse should be 9, if i use offset of 4, the mse is 16, etc.
+mse(x, x+3), mse(x, x+4), mse(x, x+12)
+# conclusion : it seems to be correct that it's the square of the offset.
+
+# 0.4.6
+# take the function f(x) from exercise 1.
+x = np.arange(201)[1:]
+plt.plot(x, f(x))
+plt.plot(x, 1.2*x)
+# visual check : it seems to be a good approximation for low values (until maybe x=100) and then the linear
+# approx. starts to be too high.
+# As it makes things a lot easier to work with a linear approximation, it could be accepted depending on the task.
+
+# 0.4.7
+mse(f(x), 1.2*x) # = 235.19621347180933
+
+# 0.4.8
+# define the range
+a = np.arange(0, 5, step=0.1)
+# calculate mse for each value of a
+mse_range = np.array([])
+for i in a:
+    new = np.array(mse(f(x), i*x))
+    mse_range = np.append(mse_range, new)
+# plot mse values for the given values of a
+plt.plot(a, mse_range)
+print('the minimum MSE value is %f and the optimal value for a is %f'
+      % (np.min(mse_range), a[np.argmin(mse_range)]))
